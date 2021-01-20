100f0d6b28bc16b7421449a050031b55c9ecca4b
realmarcin <4625870+realmarcin@users.noreply.github.com>
2021-01-19 19:08:58 -0800

    added current but not always working embedding script collection

--- embeddings/embeddings_to_tsv.py
+++ embeddings/embeddings_to_tsv.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 
-data = np.load("../IMGVR/20201205/SkipGram_embedding.npy")
+data = np.load("../IMGVR/SkipGram_embedding_IMGVR_sample.npy")
 df = pd.DataFrame(data)
-df.to_csv("../IMGVR/20201205/SkipGram_embedding.tsv", sep="\t")
+
+print(df.shape)
+
+df.to_csv("../IMGVR/SkipGram_embedding_IMGVR_sample.tsv", sep="\t")
