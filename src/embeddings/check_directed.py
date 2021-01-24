84e45e0844137dc0aa96496a472eeb9df487d5f4
realmarcin <4625870+realmarcin@users.noreply.github.com>
2021-01-24 00:53:46 -0800

    fixes

--- src/embeddings/check_directed.py
+++ src/embeddings/check_directed.py
@@ -1,20 +1,20 @@
 import pandas as pd
 
 
-edges = pd.read_csv("./data/merged/merged_kg/IMGVR_merged_final_kg_edges.tsv", sep="\t")
+edges = pd.read_csv("./IMGVR_extra_KGX_edges.tsv", sep="\t")
 dims = edges.shape
 print(dims)
 
 
 for i in range(0, dims[0]):
-    subj1 = edges.iloc[i, 1]#df['subject']
-    obj1 = edges.iloc[i, 3]# df['object']\
+    subj1 = edges.iloc[i, 0]#df['subject']
+    obj1 = edges.iloc[i, 2]# df['object']\
 
     if( i %1000 == 0):
         print(i)
 
     for j in range(i+1, dims[0]):
-         if(subj1 == edges.iloc[j, 3] and obj1 == edges.iloc[j, 1]):
+         if(subj1 == edges.iloc[j, 2] and obj1 == edges.iloc[j, 0]):
              print(subj1 + "\t" + obj1)
              print("undirected "+str(i)+"\t"+str(j))
 
