6a25bcc1c76882666b46a0357650bfe688c99428
realmarcin <4625870+realmarcin@users.noreply.github.com>
2021-01-24 18:13:59 -0800

    more efficient

--- src/embeddings/check_directed.py
+++ src/embeddings/check_directed.py
@@ -9,16 +9,15 @@ output = []
 for i in range(0, dims[0]):
     subj1 = edges.iloc[i, 0]#df['subject']
     obj1 = edges.iloc[i, 2]# df['object']\
-
     if( i %1000 == 0):
         print(i)
-
     for j in range(i+1, dims[0]):
-         if(subj1 == edges.iloc[j, 2] and obj1 == edges.iloc[j, 0]):
-             output.append(subj1 + "\t" + obj1)
-             output.append("undirected "+str(i)+"\t"+str(j))
-             print(subj1 + "\t" + obj1)
-             print("undirected "+str(i)+"\t"+str(j))
+         if(subj1 == edges.iloc[j, 2])
+             if(obj1 == edges.iloc[j, 0]):
+                 output.append(subj1 + "\t" + obj1)
+                 output.append("undirected "+str(i)+"\t"+str(j))
+                 print(subj1 + "\t" + obj1)
+                 print("undirected "+str(i)+"\t"+str(j))
 
 outfile = "check_directed_extra.txt"
 with open(outfile, "w") as outfile:
