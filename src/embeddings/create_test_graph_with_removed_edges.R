54e33a04a6ac2c7b965b0ffc4f0256f98610ed82
realmarcin <4625870+realmarcin@users.noreply.github.com>
2021-02-02 22:41:52 -0800

    positive edge removal for testing

---
+++ src/embeddings/create_test_graph_with_removed_edges.R
@@ -0,0 +1,93 @@
+rm(list=ls())
+
+library("randomForest")
+library("caTools")
+library ("ROCR")
+
+#setwd("~/graphs/KE_KG")
+#node_data <- read.csv("/global/cfs/cdirs/kbase/ke_prototype/KE_KG/data/merged/merged_imgvr_mg_nodes.tsv", sep="\t",header=T)
+setwd("~/Documents/KBase/KE/IMGVR/")
+node_data <- read.csv("./IMGVR_merged_kg_nodes.tsv", sep="\t",header=T)
+edge_data <- read.csv("./IMGVR_merged_kg_edges.tsv", sep="\t",header=T)
+
+dim(node_data)
+dim(edge_data)
+head(edge_data)
+head(node_data)
+
+#head(node_data) 
+
+node_labels <- as.character(node_data$id)
+
+test_edges <- read.csv("IMGVR_sample_extra_test_edges.txt", row.names=1, header=TRUE, sep="\t")
+class(test_edges)
+test_edges[,1]
+test_edges_split <- strsplit(as.character(test_edges[,1]), "__", fixed=TRUE)
+test_edges_split_mat <- unlist(test_edges_split)
+head(test_edges_split_mat)
+
+grep(node2, edge_data[,'subject'])
+
+cur_edges <- paste(edge_data[,'subject'], "__",edge_data[,'object'], sep="")
+head(cur_edges)
+
+
+
+test_edges_clean <- c()
+for(i in 1:length(test_edges_split)){
+  curedge <- unlist(test_edges_split[i])
+  node1 <- unlist(strsplit(curedge[1], "\t"))[2]
+  node2 <- curedge[2]
+  testedge <- paste(node1, "__", node2, sep="")
+  test_edges_clean <- c(test_edges_clean, testedge)
+}
+head(test_edges_clean)
+length(test_edges_clean)
+
+new_edges <- "subject\tpredicate\tobject"
+new_nodes <- "id\tlabel\tcategory"
+new_nodes_labels <- c()
+#edge_data_filtered <- edge_data
+for(i in 1:length(cur_edges)) {
+  oldedge <- cur_edges[i]
+  ind1 <- match(oldedge, test_edges_clean)
+  if(is.na(ind1)) {
+    split <- strsplit(oldedge, "__")
+    split1 <- unlist(split)[1]#,"\t")[2]
+    split2 <- unlist(split)[2]#,"\t")[2]
+    addedge <-  paste(split1, "\tbiolink:has_attribute\t", split2, sep="")
+    new_edges <- c(new_edges,edge_data) 
+    print(addedge)
+    if(is.na(match(split1, new_nodes_labels))) {
+      ind1 <- match(split1, node_labels)
+      new_nodes <- rbind(new_nodes, node_data[ind1,]) 
+    }
+    if(is.na(match(split2, new_nodes_labels))) {
+      ind2 <- match(split2, node_labels)
+      new_nodes <- rbind(new_nodes, node_data[ind2,])  
+    }
+  }
+}
+new_nodes <- new_nodes[-1,]
+length(new_edges)
+length(new_nodes)
+
+write.table(new_edges, file="./IMGVR_merged_kg_edges__positive80.tsv", sep="\t")
+write.table(new_edges, file="./IMGVR_merged_kg_nodes__positive80.tsv", sep="\t")
+
+
+
+
+
+
+for(i in 1:length(new_edges)){
+  data <- strsplit(cur_edge, "\t")
+  
+  ind1 <- match(data[1], new_nodes)
+  if(is.na(ind1)) {
+    split <- strsplit(curedge, "__")
+    if(match())
+    new_nodes <- c(new_nodes, curnode) 
+  }
+}
+
