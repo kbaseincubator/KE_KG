090f02a2c46aede2a23cdbe35fb7ce0f1bca8945
realmarcin <4625870+realmarcin@users.noreply.github.com>
2021-02-03 11:55:52 -0800

    faster

--- src/embeddings/create_test_graph_with_removed_edges.R
+++ src/embeddings/create_test_graph_with_removed_edges.R
@@ -3,10 +3,14 @@ rm(list=ls())
 library("randomForest")
 library("caTools")
 library ("ROCR")
+library("data.table")
+#library("fmatch")
 
-setwd("~/graphs/KE_KG")
-node_data <- read.csv("/global/cfs/cdirs/kbase/ke_prototype/graphs/IMGVR/sample_extra/IMGVR_merged_kg_nodes.tsv", sep="\t",header=T)
-edge_data <- read.csv("/global/cfs/cdirs/kbase/ke_prototype/graphs/IMGVR/sample_extra/IMGVR_merged_kg_edges.tsv", sep="\t",header=T)
+#setwd("~/graphs/KE_KG")
+#node_data <- read.csv("/global/cfs/cdirs/kbase/ke_prototype/KE_KG/data/merged/merged_imgvr_mg_nodes.tsv", sep="\t",header=T)
+setwd("~/Documents/KBase/KE/IMGVR/")
+node_data <- read.csv("./IMGVR_merged_kg_nodes.tsv", sep="\t",header=T)
+edge_data <- read.csv("./IMGVR_merged_kg_edges.tsv", sep="\t",header=T)
 
 dim(node_data)
 dim(edge_data)
@@ -17,7 +21,7 @@ head(node_data)
 
 node_labels <- as.character(node_data$id)
 
-test_edges <- read.csv("/global/cfs/cdirs/kbase/ke_prototype/graphs/IMGVR/sample_extra/IMGVR_sample_extra_test_edges.txt", row.names=1, header=TRUE, sep="\t")
+test_edges <- read.csv("IMGVR_sample_extra_test_edges.txt", row.names=1, header=TRUE, sep="\t")
 class(test_edges)
 test_edges[,1]
 test_edges_split <- strsplit(as.character(test_edges[,1]), "__", fixed=TRUE)
@@ -42,39 +46,47 @@ for(i in 1:length(test_edges_split)){
 head(test_edges_clean)
 length(test_edges_clean)
 
-new_edges <- "subject\tpredicate\tobject"
-new_nodes <- "id\tlabel\tcategory"
-new_nodes_labels <- c()
+
+
+new_edges <-  data.frame(matrix(ncol = 6, nrow = length(cur_edges)))#vector(mode="character", length=length(cur_edges))#
+colnames(new_edges) <- colnames(edge_data)#"subject\tpredicate\tobject"
+new_nodes <- data.frame(matrix(ncol = 4, nrow = length(node_labels)))#ector(mode="character", length=length(node_labels))
+colnames(new_nodes) <-  colnames(node_data)#"id\tlabel\tcategory"
+new_nodes_labels <- vector(mode="character", length=length(node_labels))#c()
+edgecount <- 0
+nodecount <- 0
 #edge_data_filtered <- edge_data
-for(i in 1:length(cur_edges)) {
+for(i in 1:200){#length(cur_edges)) {
   if( i %% 100 == 0) {
     print(i)
   }
-  oldedge <- cur_edges[i]
-  ind1 <- match(oldedge, test_edges_clean)
-  if(is.na(ind1)) {
-    split <- strsplit(oldedge, "__")
+  if(!(cur_edges[i] %chin% test_edges_clean)) {
+    split <- strsplit(cur_edges[i], "__")
     split1 <- unlist(split)[1]#,"\t")[2]
     split2 <- unlist(split)[2]#,"\t")[2]
-    addedge <-  paste(split1, "\tbiolink:has_attribute\t", split2, sep="")
-    new_edges <- c(new_edges,edge_data) 
-    #print(addedge)
-    if(is.na(match(split1, new_nodes_labels))) {
-      ind1 <- match(split1, node_labels)
-      new_nodes <- rbind(new_nodes, node_data[ind1,]) 
+    new_edges[edgecount] <- edge_data[i,]#addedge
+    edgecount <- edgecount +1
+    if(!(split1 %chin% new_nodes_labels)) {
+      ind1 <- chmatch(split1, node_labels)
+      new_nodes[nodecount] <- node_data[ind1,]
+      nodecount <- nodecount+1
     }
-    if(is.na(match(split2, new_nodes_labels))) {
-      ind2 <- match(split2, node_labels)
-      new_nodes <- rbind(new_nodes, node_data[ind2,])  
+    if(!(split2 %chin% new_nodes_labels)) {
+      ind2 <- chmatch(split2, node_labels) 
+      new_nodes[nodecount] <- node_data[ind2,]
+      nodecount <- nodecount+1
     }
   }
 }
-new_nodes <- new_nodes[-1,]
+#new_nodes <- new_nodes[-1,]
 length(new_edges)
 length(new_nodes)
 
-write.table(new_edges, file="./IMGVR_merged_kg_edges__positive80.tsv", sep="\t", row.names=FALSE, header=FALSE)
-write.table(new_edges, file="./IMGVR_merged_kg_nodes__positive80.tsv", sep="\t", row.names=FALSE, header=FALSE)
+print(edge_data[1,])
+
+
+write.table(new_edges, file="./IMGVR_merged_kg_edges__positive80.tsv", sep="\t", row.names=FALSE, col.names=FALSE)
+write.table(new_edges, file="./IMGVR_merged_kg_nodes__positive80.tsv", sep="\t", row.names=FALSE, col.names=FALSE)
 
 
 
