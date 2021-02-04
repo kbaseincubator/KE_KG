423f7a82d2e5bc9d8ac22747a30f5410be955c59
realmarcin <4625870+realmarcin@users.noreply.github.com>
2021-02-03 18:43:47 -0800

    faster

--- src/embeddings/create_test_graph_with_removed_edges.R
+++ src/embeddings/create_test_graph_with_removed_edges.R
@@ -4,6 +4,7 @@ library("randomForest")
 library("caTools")
 library ("ROCR")
 library("data.table")
+library("plyr")
 #library("fmatch")
 
 #setwd("~/graphs/KE_KG")
@@ -40,7 +41,7 @@ for(i in 1:length(test_edges_split)){
   curedge <- unlist(test_edges_split[i])
   node1 <- unlist(strsplit(curedge[1], "\t"))[2]
   node2 <- curedge[2]
-  testedge <- paste(node1, "__", node2, sep="")
+  testedge <- paste(node2, "__", node1, sep="")
   test_edges_clean <- c(test_edges_clean, testedge)
 }
 head(test_edges_clean)
@@ -55,34 +56,51 @@ colnames(new_nodes) <-  colnames(node_data)#"id\tlabel\tcategory"
 new_nodes_labels <- vector(mode="character", length=length(node_labels))#c()
 edgecount <- 0
 nodecount <- 0
+head(cur_edges)
+head(test_edges_clean)
+cur_edges_index <- match(cur_edges, test_edges_clean)
+head(cur_edges_index)
+length(cur_edges_index)
+sum(is.na(cur_edges_index))
+class(cur_edges)
+cur_edges_split <- strsplit(cur_edges, "__")#sapply(cur_edges, function(x) strsplit(x, "__")[[1]], USE.NAMES=FALSE)#ldply(
+length(cur_edges_split)
+#length(cur_edges)
+subjsplit    <- unlist(cur_edges_split)[2*(1:length(cur_edges_split))-1]
+head(subjsplit)
+objsplit  <- unlist(cur_edges_split)[2*(1:length(cur_edges_split))  ]
+head(objsplit)
+cur_edges_split_df <- data.frame(cbind(as.character(subjsplit), as.character(objsplit)),stringsAsFactors = F)
 #edge_data_filtered <- edge_data
-for(i in 1:length(cur_edges)) {
+for(i in 1:1000) {#length(cur_edges)) {
   if( i %% 100 == 0) {
     print(i)
   }
-  if(!(cur_edges[i] %chin% test_edges_clean)) {
-    split <- strsplit(cur_edges[i], "__")
-    split1 <- unlist(split)[1]#,"\t")[2]
-    split2 <- unlist(split)[2]#,"\t")[2]
+  #if(!(cur_edges[i] %chin% test_edges_clean)) {
+  if(is.na(cur_edges_index[i])) {
+    #split <- strsplit(cur_edges[i], "__")
+    #split1 <- unlist(split)[1]#,"\t")[2]
+    #split2 <- unlist(split)[2]#,"\t")[2]
     new_edges[edgecount,] <- edge_data[i,]#addedge
     edgecount <- edgecount +1
-    if(!(split1 %chin% new_nodes_labels)) {
-      ind1 <- chmatch(split1, node_labels)
+    if(!(cur_edges_split_df[i,1] %chin% new_nodes_labels)) {
+      ind1 <- chmatch(cur_edges_split_df[i,1], node_labels)
       new_nodes[nodecount,] <- node_data[ind1,]
-      new_nodes_labels[nodecount] <- split1
+      new_nodes_labels[nodecount] <- cur_edges_split_df[i, 1]
       nodecount <- nodecount+1
     }
-    if(!(split2 %chin% new_nodes_labels)) {
-      ind2 <- chmatch(split2, node_labels) 
+    if(!(cur_edges_split_df[i, 2] %chin% new_nodes_labels)) {
+      ind2 <- chmatch(cur_edges_split_df[i, 2], node_labels) 
       new_nodes[nodecount,] <- node_data[ind2,]
-      new_nodes_labels[nodecount] <- split2
+      new_nodes_labels[nodecount] <- cur_edges_split_df[i, 2]
       nodecount <- nodecount+1
     }
   }
 }
 #new_nodes <- new_nodes[-1,]
-length(new_edges)
-length(new_nodes)
+dim(new_edges)
+dim(new_nodes)
+length(node_labels)
 
 print(edge_data[1,])
 
