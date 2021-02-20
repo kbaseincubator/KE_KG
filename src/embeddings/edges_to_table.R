d28e9bf33ecafb69fe6664963e4947b21d108b65
realmarcin <4625870+realmarcin@users.noreply.github.com>
2021-02-19 18:21:15 -0800

    filtering to only edges with sample subject (for row primary key)

--- src/embeddings/edges_to_table.R
+++ src/embeddings/edges_to_table.R
@@ -11,6 +11,20 @@ dim <- dim(edge_data)
 long_data <- cbind(edge_data[,c("subject", "object")], rep(1, dim[1]))
 colnames(long_data) <- c("subject", "object", "value")
 head(long_data)
+
+samples_subject <- grep("GOLD:ga", long_data$subject)
+samples_object <- grep("GOLD:ga", long_data$object)
+
+length(samples_subject)
+length(samples_object)
+
+#removing edges from sample to sample
+long_data <- long_data[-samples_object,]
+samples_subject <- grep("GOLD:ga", long_data$subject)
+#only pick sample subjects
+long_data <- long_data[samples_subject,]
+dim(long_data)
+
 wide_data <- reshape(data=long_data, idvar="subject", timevar="object", direction="wide")
 
 write.table(wide_data, file="IMGVR_merged_kg_table.tsv", sep="\t")
