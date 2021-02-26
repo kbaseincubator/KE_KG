7da23ee70661eccb1f02625173a4f66d1f77532b
realmarcin <4625870+realmarcin@users.noreply.github.com>
2021-02-25 23:22:39 -0800

    index bug

--- src/embeddings/virus_host_v2_80_kgmicrobe.R
+++ src/embeddings/virus_host_v2_80_kgmicrobe.R
@@ -50,9 +50,9 @@ head(all_objects)
 #head(objects)
 
 
-subjects <- train_edges$subject
+subjects <- train_edges$object
 head(subjects)
-objects  <- train_edges$object
+objects  <- train_edges$subject
 head(objects)
 
 length(subjects)
@@ -96,7 +96,7 @@ if(!done) {
     }
     
     if(!is.na(objects_full_index[i])){
-      curlabel <- paste(node_labels[subjects_index[i]],"__",node_labels[objects_full_index[i]],sep="")
+      curlabel <- paste(node_labels[subjects_index[i]],"__",node_labels[objects_index[i]],sep="")
       #print(curlabel)
       if(length(curlabel) > 0 && !(curlabel %in% train_edges__subtract_label)) {
         print(curlabel)
