bd2aacd49d5b33ebdad3ecf6a0940a09e844b765
realmarcin <4625870+realmarcin@users.noreply.github.com>
2021-03-18 11:07:30 -0700

    path

--- src/from_raw/mgnify_samples_to_dict.py
+++ src/from_raw/mgnify_samples_to_dict.py
@@ -5,7 +5,7 @@ import os
 path = "/global/cfs/cdirs/kbase/KE-Catboost/jrb/data/mgnify/samples"
 dict_all = dict()
 for x in os.listdir(path):
-    with open(path+"/"+x) as json_file:
+    with open(path+"/"+x+"/sample.json") as json_file:
         curdict = json.load(json_file)
         dict_all.update(curdict)
 
