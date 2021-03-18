6b7cbda30770c024c56d0e95ea134076f7b99f6c
realmarcin <4625870+realmarcin@users.noreply.github.com>
2021-03-18 11:39:56 -0700

    also key count dict

--- src/from_raw/mgnify_samples_to_dict.py
+++ src/from_raw/mgnify_samples_to_dict.py
@@ -4,6 +4,7 @@ import os
 
 path = "/global/cfs/cdirs/kbase/KE-Catboost/jrb/data/mgnify/samples"
 dict_all = dict()
+dict_count = dict()
 for x in os.listdir(path):
     print(x)
     with open(path+"/"+x+"/sample.json") as json_file:
@@ -12,24 +13,27 @@ for x in os.listdir(path):
 
         #print(curdata['attributes'])
 
-        for key in curdata['attributes']:
-            print(key+"\t"+str(curdata['attributes'][key]))
+        #for key in curdata['attributes']:
+        #    print(key+"\t"+str(curdata['attributes'][key]))
 
-        curdict = dict()
 
         for key in curdata['attributes']:
-            print(key)
+            #print(key)
             if key in dict_all:
                 if str(curdata['attributes'][key]) not in str(dict_all[key]):
                     dict_all[key] = str(dict_all[key]) + " ** " + str(curdata['attributes'][key])
             else:
                 dict_all[key] = curdata['attributes'][key]
 
-        #curdict['type'] = curdata['type']
-        #curdict['id'] = curdata['id']
+            if key in dict_count:
+                dict_count[key] = dict_count[key] + 1
+            else:
+                dict_count[key] =  1
 
-        #dict_all.update(curdict)
 
+f = open("mgnify_sample_key_count.txt","w")
+f.write( str(dict_count) )
+f.close()
 
 f = open("mgnify_sample_dict.txt","w")
 f.write( str(dict_all) )
