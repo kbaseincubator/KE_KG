6904f1c406c827d20969dd2ddb2f3f7c4e458b54
realmarcin <4625870+realmarcin@users.noreply.github.com>
2021-03-18 11:06:30 -0700

    append dicts and save

--- src/from_raw/mgnify_samples_to_dict.py
+++ src/from_raw/mgnify_samples_to_dict.py
@@ -2,7 +2,14 @@
 import json
 
 
+path = "/global/cfs/cdirs/kbase/KE-Catboost/jrb/data/mgnify/samples"
+dict_all = dict()
+for x in os.listdir(path):
+    with open(path+"/"+x) as json_file:
+        curdict = json.load(json_file)
+        dict_all.update(curdict)
 
-with open('data.json') as json_file:
-        data = json.load(json_file)
-                                
+
+f = open("mgnify_sample_dict.txt","w")
+f.write( str(dict_all) )
+f.close()
