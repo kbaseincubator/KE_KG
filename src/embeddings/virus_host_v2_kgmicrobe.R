22b06d111a08c02f89dbcf89cb9d834ece092721
realmarcin <4625870+realmarcin@users.noreply.github.com>
2021-04-20 18:57:40 -0700

    path issue

--- src/embeddings/virus_host_v2_kgmicrobe.R
+++ src/embeddings/virus_host_v2_kgmicrobe.R
@@ -2,22 +2,22 @@ rm(list=ls())
 set.seed(12345)
 setwd("/global/scratch/marcin/N2V/KE_KG/")
 
-embeddings <- read.csv("/global/scratch/marcin/N2V/embiggen/notebooks/kg_microbe/20210315/SkipGram_embedding_kgmicrobe.csv", sep=",", header=TRUE, row.names=1)
+embeddings <- read.csv("/global/scratch/marcin/N2V/embiggen/notebooks/kg_microbe/20210119/SkipGram_embedding_kgmicrobe_new_embedding_labels.tsv", sep="\t", header=TRUE, row.names=1)
 head(embeddings)
 dim(embeddings)
 #node_data <- read.csv("../KE_KG/data/merged_last/IMGVR_merged_final_KGX_nodes.tsv", sep="\t",header=T)
-node_data <- read.csv("/global/scratch/marcin/N2V/embiggen/notebooks/kg_microbe/20210315/merged-kg_nodes.tsv", sep="\t",header=T, quote="", stringsAsFactors = FALSE)
+node_data <- read.csv("/global/scratch/marcin/N2V/embiggen/notebooks/kg_microbe/20210119/merged-kg_nodes.tsv", sep="\t",header=T, quote="", stringsAsFactors = FALSE)
 dim(node_data)
 head(node_data) 
 
 node_labels <- as.character(node_data$id)
 
 
-train_edges <- read.csv("/global/scratch/marcin/N2V/embiggen/notebooks/kg_microbe/20210315/merged-kg_edges_Shape_whead.tsv",sep="\t",header=T)
+train_edges <- read.csv("/global/scratch/marcin/N2V/embiggen/notebooks/kg_microbe/20210119/merged_kg_edges_Shape_whead.tsv",sep="\t",header=T)
 dim(train_edges)
 head(train_edges) 
 
-all_objects <-  read.csv("/global/scratch/marcin/N2V/embiggen/notebooks/kg_microbe/20210315/merged-kg_nodes_NCBITaxon_whead.tsv",sep="\t",header=T)
+all_objects <-  read.csv("/global/scratch/marcin/N2V/embiggen/notebooks/kg_microbe/20210119/merged-kg_nodes_NCBITaxon_whead.tsv",sep="\t",header=T)
 dim(all_objects)
 head(all_objects) 
 
