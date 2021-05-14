8abc95f0e6d32f8c015c6dde9a3c82e27ee921ad
realmarcin <4625870+realmarcin@users.noreply.github.com>
2021-05-13 23:27:04 -0700

    compute only half jaccard matrix

--- src/embeddings/compare_searches.R
+++ src/embeddings/compare_searches.R
@@ -1,5 +1,6 @@
 
-rm(list=ls())
+
+rm(list = ls())
 
 library("plyr")
 library("gplots")
@@ -15,33 +16,33 @@ all_names <- c()
 data <- list()#data.frame(matrix(NA, nrow = length(files)))
 count <- 1
 lastgood <- data.frame()
-for(i in 1:length(files)) {
-  if(i %% 1000 == 0) {
+for (i in 1:length(files)) {
+  if (i %% 1000 == 0) {
     print(i)
   }
-  datanow <- read.csv(files[i], sep="\t")
+  datanow <- read.csv(files[i], sep = "\t")
   dimd1 <- dim(datanow)
   #print(dimd1[1])
   #print(head(datanow))
-  datanow <- datanow[which(datanow[,2] > 0.8),]
+  datanow <- datanow[which(datanow[, 2] > 0.8), ]
   dimd2 <- dim(datanow)
   #print(dimd2[1])
   
   #cosine__NCBItaxon:;;;;;alphasatellitidae;;_top10000_cutoff0.1.txt
-  start <- gregexpr(pattern ='__',files[i])[[1]][1]
-  end <- gregexpr(pattern ='_top10',files[i])[[1]][1]
-  name <- substr(files[i], start+2, end-1)
+  start <- gregexpr(pattern = '__', files[i])[[1]][1]
+  end <- gregexpr(pattern = '_top10', files[i])[[1]][1]
+  name <- substr(files[i], start + 2, end - 1)
   #print(name)
   
-  if(dimd2[1] > 0) {
+  if (dimd2[1] > 0) {
     #print("adding")
     #print(dimd1[1] - dimd2[1])
     #data <- rbind(data, as.character(datanow[,1]))
-    data[[count]] <- as.character(datanow[,1])
+    data[[count]] <- as.character(datanow[, 1])
     #print(data[[count]])
     #data[i,] <- datanow[,1]
     names <- c(names, name)
-    count <- count+1
+    count <- count + 1
     lastgood <- datanow
   }
   all_names <- c(all_names, name)
@@ -52,7 +53,11 @@ length(data)
 data_unlist_all <- unlist(data)
 length(data_unlist_all)
 
-df <- do.call(rbind,lapply(data,function(x) "length<-"(x,max(lengths(data)))))
+df <-
+  do.call(rbind, lapply(data, function(x)
+    "length<-"(x, max(lengths(
+      data
+    )))))
 dim(df)
 
 head(df)
@@ -68,29 +73,32 @@ head(df)
 jaccard <- function(a, b) {
   intersection = length(intersect(a, b))
   union = length(a) + length(b) - intersection
-  return (intersection/union)
+  return (intersection / union)
 }
 
 
 head(df)
 dim_df <- dim(df)
-pairwise_jaccard<- data.frame(matrix(NA, nrow = dim_df[1], ncol=dim_df[1]))
-for(i in 1:dim_df[1]) {
-  if(i %% 10000 == 0) {
+pairwise_jaccard <-
+  data.frame(matrix(NA, nrow = dim_df[1], ncol = dim_df[1]))
+for (i in 1:dim_df[1]) {
+  if (i %% 10000 == 0) {
     print(i)
   }
-  dfi <- df[i,][which(!is.na(df[i,]))]
-  for(j in i:dim_df[1]) {
-    dfj <- df[j,][which(!is.na(df[j,]))]
-    if(i != j) {
-      pairwise_jaccard[i, j] <- jaccard(dfi, dfj)
-      pairwise_jaccard[j, i] <- pairwise_jaccard[i, j]
-      #intersect <- intersect(dfi, dfj)
-      #if(length(intersect) > 0) {
-      #  outf <- paste(names[i], "_", names[j],"_common.txt",sep="")
-      #  print(outf)
-      #  write.table(intersect, file=outf,sep="\t")
-      #}
+  dfi <- df[i, ][which(!is.na(df[i, ]))]
+  for (j in i:dim_df[1]) {
+    dfj <- df[j, ][which(!is.na(df[j, ]))]
+    if (i != j) {
+      if (i > j) {
+        pairwise_jaccard[i, j] <- jaccard(dfi, dfj)
+        pairwise_jaccard[j, i] <- pairwise_jaccard[i, j]
+        #intersect <- intersect(dfi, dfj)
+        #if(length(intersect) > 0) {
+        #  outf <- paste(names[i], "_", names[j],"_common.txt",sep="")
+        #  print(outf)
+        #  write.table(intersect, file=outf,sep="\t")
+        #}
+      }
     }
     else {
       pairwise_jaccard[i, j] <- 0
@@ -101,7 +109,13 @@ for(i in 1:dim_df[1]) {
 row.names(pairwise_jaccard) <- names
 colnames(pairwise_jaccard) <- names
 
-write.table(pairwise_jaccard, file="pairwise_jaccard.tsv",sep="\t", row.names=F, col.names=F)
+write.table(
+  pairwise_jaccard,
+  file = "pairwise_jaccard.tsv",
+  sep = "\t",
+  row.names = F,
+  col.names = F
+)
 print("wrote tsv")
 #write.table(names, file="GOLD_names.txt",row.names=F, col.names=F)
 #write.table(names_all, file="GOLD_names_all.txt",row.names=F, col.names=F)
@@ -110,7 +124,7 @@ library(RColorBrewer)
 library(ggplot2)
 library(pheatmap)
 library(amap)
-library(gplots) 
+library(gplots)
 
 
 cellwidth <- 1
@@ -124,7 +138,7 @@ fontsize_col <- 5
 range <- range(pairwise_jaccard)
 mypalette <- rev(brewer.pal(4, "Blues"))
 mypalette <- c(mypalette, brewer.pal(4, "YlOrBr"))
-breaks <- seq(0, range[2],range[2]/8)
+breaks <- seq(0, range[2], range[2] / 8)
 
 
 #heatmap.2(as.matrix(pairwise_jaccard), trace="none")#log(pairwise_jaccard + 0.001, 10))
@@ -132,10 +146,25 @@ breaks <- seq(0, range[2],range[2]/8)
 
 
 
-png(filename=paste("clustering_cosine_jaccard_0.8.png",sep=""),width=1200, height=800)
-pheatmap(as.matrix(virus_host_positive),scale = "none", cluster_rows = TRUE,
-         cluster_cols = TRUE, clustering_distance_rows = "euclidean",
-         clustering_distance_cols = "euclidean", clustering_method = "complete",cellwidth=cellwidth,cellheight=cellheight,breaks=breaks,color=mypalette,show_rownames=F,show_colnames=F,legend=F)#
+png(
+  filename = paste("clustering_cosine_jaccard_0.8.png", sep = ""),
+  width = 1200,
+  height = 800
+)
+pheatmap(
+  as.matrix(virus_host_positive),
+  scale = "none",
+  cluster_rows = TRUE,
+  cluster_cols = TRUE,
+  clustering_distance_rows = "euclidean",
+  clustering_distance_cols = "euclidean",
+  clustering_method = "complete",
+  cellwidth = cellwidth,
+  cellheight = cellheight,
+  breaks = breaks,
+  color = mypalette,
+  show_rownames = F,
+  show_colnames = F,
+  legend = F
+)#
 dev.off(2)
-
-
