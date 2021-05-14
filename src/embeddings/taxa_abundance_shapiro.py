6540e642e80640ba7ce0e2994f3d5862fdf50b64
realmarcin <4625870+realmarcin@users.noreply.github.com>
2021-05-13 23:33:31 -0700

    print

--- src/embeddings/taxa_abundance_shapiro.py
+++ src/embeddings/taxa_abundance_shapiro.py
@@ -8,6 +8,8 @@ import os
 
 df_taxa_orig = pd.read_csv("../../../MGnify/taxonomy_aggregated_full_removed_duplicates.tsv", sep="\t")
 
+print("read input")
+
 cols = [col for col in df_taxa_orig.columns if col not in ['id', 'study_id', 'sample_id', 'biome', 'exptype']]
 df_taxa = df_taxa_orig[cols]
 df_taxa.shape
@@ -34,7 +36,10 @@ for col in df_taxa.columns:
     #if(count > limit):
     #    break
 
+print("done shapiro")
 shapiro_out_df = pd.DataFrame(shapiro_out, columns=["stat", "p"], dtype=np.float64)
+print("done shapiro df")
 shapiro_out_df.shape
 
 shapiro_out_df.to_csv("df_taxa_col_shapiro.tsv", sep="\t")
+print("done tsv")
