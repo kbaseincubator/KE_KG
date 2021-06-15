8b5bf4bbe89b31e9caa03e5c7aa417dfb5e4e811
realmarcin <4625870+realmarcin@users.noreply.github.com>
2021-06-14 18:58:48 -0700

    checking for constant columns

--- src/embeddings/taxa_abundance_pearson.py
+++ src/embeddings/taxa_abundance_pearson.py
@@ -23,31 +23,33 @@ for i in range(0, df_taxa.columns.size):
 #for col in df_taxa.columns:
     col = df_taxa.columns[i]
     #colind = df_taxa.columns.index(col)
+    if(df_taxa[col][df_taxa[col] > 0]):
+        for j in range(i+1, df_taxa.columns.size):
+            # for col2 in df_taxa.columns:
+            col2 = df_taxa.columns[j]
+            # col2ind = df_taxa.columns.index(col2)
+            # print(col2)
+            if (df_taxa[col2][df_taxa[col2] > 0]):
 
-    for j in range(i+1, df_taxa.columns.size):
-        # for col2 in df_taxa.columns:
-        col2 = df_taxa.columns[j]
-        #col2ind = df_taxa.columns.index(col2)
-        #print(col)
-        stat, p = pearsonr(df_taxa[col], df_taxa[col2])
+                stat, p = pearsonr(df_taxa[col], df_taxa[col2])
 
-        if (count % 100 == 0):
-            print("count " + str(count))
-            print("stat, p %s %s" % (stat, p))
-            print(df_taxa[col]+"\t"+df_taxa[col2])
+                if (count % 100 == 0):
+                    print("count " + str(count))
+                    print("stat, p %s %s" % (stat, p))
+                    print(df_taxa[col]+"\t"+df_taxa[col2])
 
 
-        #print(df_taxa[col])
-        pearson_out.append([i, j, stat, p])
-        pearson_out.append([j, i, stat, p])
+                #print(df_taxa[col])
+                pearson_out.append([i, j, stat, p])
+                pearson_out.append([j, i, stat, p])
 
-        #print("%s %s" % (stat,p))
-        #curtime = time.process_time()
-        #print(curtime - last)
-        #last = curtime
-        count = count + 1
-        #if(count > limit):
-        #    break
+                #print("%s %s" % (stat,p))
+                #curtime = time.process_time()
+                #print(curtime - last)
+                #last = curtime
+                count = count + 1
+                #if(count > limit):
+                #    break
 
 print("done pearson")
 pearson_out_df = pd.DataFrame(pearson_out, columns=["col1", "col2", "pearson_stat", "pearson_p"], dtype=np.float64)
