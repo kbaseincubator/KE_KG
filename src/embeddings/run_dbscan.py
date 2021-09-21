d6328648dafe5b10ea1222c3cf3e9f4a4856411d
realmarcin <4625870+realmarcin@users.noreply.github.com>
2021-09-20 18:27:19 -0700

    run dbscan

---
+++ src/embeddings/run_dbscan.py
@@ -0,0 +1,28 @@
+import os
+
+os.getcwd()
+
+os.chdir('/global/cfs/cdirs/kbase/ke_prototype/KE_KG')
+
+from sklearn.cluster import DBSCAN
+from sklearn.cluster import AgglomerativeClustering
+import numpy as np
+from numpy import genfromtxt
+import pandas
+
+
+#skip column names
+data = genfromtxt('../embeddings/SkipGram_embedding_merged_imgvr_mg_good.csv', delimiter=',',skip_header=1)#,names=True
+
+data.shape
+#remove row names from numpy array
+data = data[:,1:]
+data.shape
+
+clustering = DBSCAN(eps=3, min_samples=2).fit(data.astype(float))
+
+
+np.savetxt('dbscan.txt', clustering, delimiter='\t')
+np.savetxt('dbscan_cluster_labels.txt', clustering.labels_, delimiter='\t')
+
+clustering.labels_
