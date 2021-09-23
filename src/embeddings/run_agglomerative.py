1552c31cc5ac756818b2b8856bb1e9305dcc08df
realmarcin <4625870+realmarcin@users.noreply.github.com>
2021-09-23 12:41:54 -0700

    added dendrogram plotting method

--- src/embeddings/run_agglomerative.py
+++ src/embeddings/run_agglomerative.py
@@ -36,6 +36,27 @@ with open('agglomerative_cluster_labels.txt', 'w') as f:
 
 
 
+def plot_dendrogram(model, **kwargs):
+    # Create linkage matrix and then plot the dendrogram
+
+    # create the counts of samples under each node
+    counts = np.zeros(model.children_.shape[0])
+    n_samples = len(model.labels_)
+    for i, merge in enumerate(model.children_):
+        current_count = 0
+        for child_idx in merge:
+            if child_idx < n_samples:
+                current_count += 1  # leaf node
+            else:
+                current_count += counts[child_idx - n_samples]
+        counts[i] = current_count
+
+    linkage_matrix = np.column_stack([model.children_, model.distances_,
+                                      counts]).astype(float)
+
+    # Plot the corresponding dendrogram
+    dendrogram(linkage_matrix, **kwargs)
+
 plt.title('Hierarchical Clustering Dendrogram - top 5 levels')
 # plot the top three levels of the dendrogram
 plot_dendrogram(model, truncate_mode='level', p=5)
