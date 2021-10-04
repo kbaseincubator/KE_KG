d6f022df46351aa2d2aeada8dce3268c12cb9806
realmarcin <4625870+realmarcin@users.noreply.github.com>
2021-10-04 11:46:08 -0700

    fix reading cosine sim data

--- src/embeddings/run_dendrogram.py
+++ src/embeddings/run_dendrogram.py
@@ -4,7 +4,12 @@ import scipy.cluster.hierarchy as shc
 from matplotlib import pyplot
 from numpy import genfromtxt
 
-data = genfromtxt('../embeddings/SkipGram_embedding_merged_imgvr_mg_good.csv', delimiter=',')
+data = genfromtxt('../embeddings/SkipGram_embedding_merged_imgvr_mg_good.csv', delimiter=',', skip_header=1)
+
+data.shape
+#remove row labels
+data = data[:,1:]
+data.shape
 
 pyplot.figure(figsize=(10, 7))  
 pyplot.title("Dendrogram")
