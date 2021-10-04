7c60a05b9cf26ef9857a5592ad04ea2aa2a75329
realmarcin <4625870+realmarcin@users.noreply.github.com>
2021-10-04 16:34:38 -0700

    try for GO

--- src/embeddings/run_dendrogram.py
+++ src/embeddings/run_dendrogram.py
@@ -4,11 +4,15 @@ import scipy.cluster.hierarchy as shc
 from matplotlib import pyplot
 from numpy import genfromtxt
 
-data = genfromtxt('../embeddings/SkipGram_embedding_merged_imgvr_mg_good.csv', delimiter=',', skip_header=1)
+#data = genfromtxt('../embeddings/SkipGram_embedding_merged_imgvr_mg_good.csv', delimiter=',', skip_header=1)
+
+data = genfromtxt('../../KE-Catboost/ziming/GO/data/go_aggregated_4.1/go_aggregated_4.1_mixed_updated_normalized_removed_zeros.tsv', delimiter='\t', skip_header=1)
+
+
 
 data.shape
 #remove row labels
-data = data[:,1:]
+data = data[:,8:]
 data.shape
 
 pyplot.figure(figsize=(10, 7))  
